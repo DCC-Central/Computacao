f27b7257f51bc7c09241a4285c6dd6a75d52f42a
Leonardo Dantas <leonardosdantas@protonmail.com>
2016-04-08 16:48:59 -0300

    Aula 08/04/16

---
+++ aulas-praticas/08-04-16/calculaMedia.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main ( void ) {
+  float nota1,
+        nota2,
+        media;
+
+  printf("Este programa calcula a média de um aluno, dado duas notas.\n\n");
+  printf("Digite a primeira nota: ");
+  scanf("%f", ¬a1);
+  printf("Digite a segunda nota: ");
+  scanf("%f", ¬a2);
+
+  media = (nota1 + nota2)/2;
+
+  printf("\nSua média é igual a: %1.3f\n", media);
+
+  if (media >= 5) {
+    printf("Você foi aprovado!\n");
+  }
+  else {
+    printf("Você foi reprovado!\n");
+  }
+
+  return 0;
+}
---
+++ aulas-praticas/08-04-16/primeiro.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ( void ) {
+  float a, / coeficiente da equação de primeiro grau /
+        b, / termo independente da equação /
+        x; / resultado da equação de primeiro grau /
+
+  printf("Este programa resolve uma equação de primeiro grau. \n\n");
+  printf("Entre com o coeficiente a, por favor: ");
+  scanf("%f", &a);
+  printf("Entre com o coeficiente b, por favor: ");
+  scanf("%f", &b);
+
+  if (a == 0) {
+    printf("\nEsta equação nao tem solução. \n");
+  }
+  else {
+    x = -b/a;
+    printf("\nA solução da equação vale %1.3f\n",x);
+  }
+
+  return 0;
+}
